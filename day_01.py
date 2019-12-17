8a5bc447be766708f74721b88842acf8a43255e0
Imploder <josh.lr.allen@gmail.com>
2019-12-17 23:36:32 +0000

    Calculate fuel for added fuel
    - Refactored calculation to calculate modules individually
    - Added function to calculate total fuel needed for all extra fuel added

--- day_01.py
+++ day_01.py
@@ -6,15 +6,20 @@ def read_input_file(input_file):
         return [word.strip() for word in file.read().split()]
 
 
-def calculate_module_fuel_total(modules):
-    total = 0
-    for module in modules:
-        fuel_required = (int(module) // 3) - 2
-        total += fuel_required
+def calculate_fuel_needed_for_mass(mass):
+    return (int(mass) // 3) - 2
 
-    return total
+
+def calculate_total_fuel_needed_for_mass(mass):
+    total_fuel = 0
+    incremental_fuel = calculate_fuel_needed_for_mass(mass)
+    while incremental_fuel >= 0:
+        total_fuel += incremental_fuel
+        incremental_fuel = calculate_fuel_needed_for_mass(incremental_fuel)
+
+    return total_fuel
 
 
 if __name__ == '__main__':
-    module_mass = read_input_file(raw_data)
-    print(calculate_module_fuel_total(module_mass))
+    mass_of_modules = read_input_file(raw_data)
+    print(sum(calculate_total_fuel_needed_for_mass(int(module)) for module in mass_of_modules))
